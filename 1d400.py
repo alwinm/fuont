297c46757edaf1e30fe7364a2559dd2d6ceb3842
Alwin <alwin.mao@pitt.edu>
2022-06-15 02:08:55 -0700

    fill unicode holes

--- 1d400.py
+++ 1d400.py
@@ -1,8 +1,10 @@
+from fuont.holes import chrf
+
 hexA = '0x1d400'
 intA = int(hexA,16)
 
 for i in range(40):
     base = intA + i*26
-    print(hex(base),''.join([chr(base+i) for i in range(26)]))
+    print(hex(base),''.join([chrf(base+i) for i in range(26)]),'\n')
 
     
--- alpha.py
+++ alpha.py
@@ -1,7 +1,9 @@
 import sys
 import string
+from fuont.holes import chrf
+
 print('Usage: python -m fuont.alpha [hex code of letter A]')
 
 hexA = sys.argv[1]
 
-print(''.join([chr(int(hexA,16)+i) for i in range(26)]))
+print(''.join([chrf(int(hexA,16)+i) for i in range(26)]))
